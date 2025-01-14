2234f1080a678bbff3e1a290ae5e97703fd4f1d8
Lody Borgers <hi@lodybo.nl>
2025-01-14 11:39:51 +0100

    Fixed the date test spec to use mocked timers, now the test should run the same everywhere.

--- app/utils/date.test.ts
+++ app/utils/date.test.ts
@@ -1,9 +1,18 @@
-import { describe, test, expect } from 'vitest';
+import { describe, test, expect, beforeEach, afterEach, vi } from 'vitest';
 import { convertDateToLocaleString, convertTimeToLocaleString } from './date';
 
 describe('convertDateToLocaleString', () => {
+  beforeEach(() => {
+    vi.useFakeTimers({});
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
   test('converts a date to a pretty printed string', () => {
     const date = new Date('2024-04-08T00:00:00Z');
+    vi.setSystemTime(date);
     const result = convertDateToLocaleString(date);
     expect(result).toBe('08-04-2024');
   });
@@ -11,7 +20,7 @@ describe('convertDateToLocaleString', () => {
   test('converts a time to a pretty printed string', () => {
     const date = new Date('2024-04-08T10:30:00Z');
     const result = convertTimeToLocaleString(date);
-    expect(result).toBe('12:30');
-    // expect(result).toBe('16:00'); // Set to 16 because of Indian timezone, need to mock Date constructor for tests.
+    vi.setSystemTime(date);
+    expect(result).toBe('10:30');
   });
 });
