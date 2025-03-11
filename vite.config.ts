9a3847a3dbf202939355a4d1e40d21ae2fa99d36
Lody Borgers <hi@lodybo.nl>
2025-03-11 15:09:49 +0100

    Turned on the future flag for route config.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { envOnlyMacros } from 'vite-env-only';
-import { flatRoutes } from 'remix-flat-routes';
 import react from '@vitejs/plugin-react';
 
 const isStorybook = process.argv[1]?.includes('storybook');
@@ -20,15 +19,13 @@ export default defineConfig({
     isStorybook ? react() : (
       remix({
         ignoredRouteFiles: ['**/*'],
-        routes: async (defineRoutes) => {
-          return flatRoutes('routes', defineRoutes);
-        },
         future: {
           v3_fetcherPersist: true,
           v3_relativeSplatPath: true,
           v3_throwAbortReason: true,
           v3_lazyRouteDiscovery: true,
           v3_singleFetch: true,
+          v3_routeConfig: true,
         },
       })
     ),
