63e54751cdba20a911d19a76791cdeb461e990a8
Lody Borgers <hi@lodybo.nl>
2025-04-23 15:25:13 +0200

    Implemented a file storage for DO Spaces for dev/prod

--- app/.server/file-uploads.ts
+++ app/.server/file-uploads.ts
@@ -1,7 +1,16 @@
 import { type FileUpload, parseFormData } from '@mjackson/form-data-parser';
 import { LocalFileStorage } from '@mjackson/file-storage/local';
+import { FileStorage } from '@mjackson/file-storage';
+import { S3FileStorageClass } from '~/.server/S3FileStorageClass';
 
-export const fileStorage = new LocalFileStorage('./tmp/uploads');
+let storage: FileStorage;
+console.log('Setting up file storage', process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production') {
+  storage = new S3FileStorageClass();
+} else {
+  storage = new LocalFileStorage('./tmp/uploads');
+}
+export const fileStorage = storage;
 
 async function uploadHandler(fieldName: string, fileUpload: FileUpload) {
   if (
