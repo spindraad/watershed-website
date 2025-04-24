da58d33afa11ca2d4ef2bf49850dcc68a68d0018
Lody Borgers <hi@lodybo.nl>
2025-04-24 10:58:59 +0200

    feature/WAT 168 Implement S3 Deployment (#30)
    * Configured Kubernetes templates with the correct bucket name and credentials based on the deployment environment
    
    * Implemented a file storage for DO Spaces for dev/prod
    
    * Implemented a file storage for DO Spaces for dev/prod

--- app/routes/($lang)/beheer/media/_index/_media-overview.tsx
+++ app/routes/($lang)/beheer/media/_index/_media-overview.tsx
@@ -2,10 +2,20 @@ import { FormEvent, useContext, useState } from 'react';
 import Heading from '~/components/Heading';
 import { ShoelaceContext } from '~/components/shoelace';
 import Input from '~/components/Input';
+import { fileStorage } from '~/.server/file-uploads';
+import { useLoaderData } from 'react-router';
+
+export async function loader() {
+  const { files } = await fileStorage.list({ includeMetadata: true });
+
+  return {
+    files,
+  };
+}
 
 export default function MediaIndexRoute() {
   const { SlButton } = useContext(ShoelaceContext);
-
+  const { files } = useLoaderData<typeof loader>();
   const [fileName, setFileName] = useState('');
 
   const onFormSubmit = (event: FormEvent<HTMLFormElement>) => {
@@ -18,6 +28,22 @@ export default function MediaIndexRoute() {
   return (
     <div className="space-y-4">
       <Heading level={1}>Media</Heading>
+
+      <ul className="list-none space-y-6">
+        {files.map((file) => (
+          <li key={file.key} className="flex flex-row items-center gap-2">
+            <img
+              src={`/afbeelding/${file.name}`}
+              alt={file.name}
+              className="w-16 h-16 object-cover"
+            />
+            <div className="flex flex-col">
+              <span>{file.name}</span>
+              <span className="text-sm text-gray-500">{file.size} bytes</span>
+            </div>
+          </li>
+        ))}
+      </ul>
 
       <form className="space-y-1" onSubmit={onFormSubmit}>
         <Input label="Filename to retrieve" name="filename" type="text" />
--- environment.d.ts
+++ environment.d.ts
@@ -4,5 +4,8 @@ declare namespace NodeJS {
     DATABASE_URL: string;
     SENDGRID_API_KEY: string;
     SESSION_SECRET: string;
+    BUCKET_NAME: string;
+    BUCKET_KEY_ID: string;
+    BUCKET_SECRET_KEY: string;
   }
 }
