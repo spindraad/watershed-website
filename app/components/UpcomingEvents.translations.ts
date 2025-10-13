538693ed8b761807bc30e7676e5dbb3e42c202b4
Lody Borgers <hi@lodybo.nl>
2025-10-13 13:48:37 +0200

    Improved the upcoming events

---
+++ app/components/UpcomingEvents.translations.ts
@@ -0,0 +1,17 @@
+export const en = {
+  UpcomingEventsComponent: {
+    altTextIllustration: 'Black and white drawing of a donkey',
+  },
+};
+
+export const nl = {
+  UpcomingEventsComponent: {
+    altTextIllustration: 'Zwart-wit tekening van een ezel',
+  },
+};
+
+export const pap = {
+  UpcomingEventsComponent: {
+    altTextIllustration: 'Dibuho di buriku na blanku i shelu',
+  },
+};
--- app/components/UpcomingEvents.tsx
+++ app/components/UpcomingEvents.tsx
@@ -1,6 +1,9 @@
 import Heading from '~/components/Heading';
 import Icon from '~/components/Icon';
 import type { Event } from '~/models/events.server';
+import { useTranslation } from 'react-i18next';
+import HandDrawnBox from '~/components/HandDrawnBox';
+import PostItNote from '~/components/PostItNote';
 
 export type UpcomingEventDetails = Pick<
   Event,
@@ -12,35 +15,57 @@ type Props = {
 };
 
 export default function UpcomingEvents({ events }: Props) {
+  const { t } = useTranslation('UpcomingEvents');
+
+  const formatDate = (date: Date) =>
+    date.toLocaleDateString('nl-NL', {
+      weekday: 'short', // "wo"
+      day: 'numeric', // "11"
+      month: 'short', // "sept"
+    });
+
   return (
     <div>
       <div className="flex flex-row justify-between items-center">
         <div className="flex flex-row gap-2 items-center">
           <Icon name="heading-arrow" />
-          <Heading level={2}>coming UP</Heading>
+          <Heading level={2} textSizeClass="text-xl">
+            coming UP
+          </Heading>
         </div>
 
         <Gimmick />
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
+      <ul className="grid grid-cols-[repeat(2,minmax(10vw,20rem))] gap-4 mt-8">
+        <li className="w-36 sm:w-44 h-auto">
+          <img
+            src="/illustraties/ezel-ZW.png"
+            alt={t('altTextIllustration')}
+            className="w-full h-auto sm:w-44 sm:h-44"
+          />
+        </li>
         {events.map((event) => (
-          <div key={event.id} className="flex flex-col gap-2">
-            <img
-              src={`/afbeelding/${event.image}`}
-              alt={event.title.en}
-              className="w-24 h-auto rounded-lg"
-            />
-            <h3 className="text-xl font-bold">{event.title.en}</h3>
+          <li key={event.id} className="w-36 h-auto flex flex-col gap-2">
+            <HandDrawnBox drawStyle="dotted" padding="p-3" classes="relative">
+              <img
+                src={`/afbeelding/${event.image}`}
+                alt={event.title.en}
+                className="w-36 h-36 object-cover"
+              />
+              <PostItNote classes="absolute bottom-4 -right-4 text-xs">
+                {formatDate(event.eventDate)}
+              </PostItNote>
+            </HandDrawnBox>
+            <Heading level={3} textSizeClass="text-base" bold={false}>
+              {event.title.en}
+            </Heading>
             {event.organiser && (
               <p className="text-sm text-gray-500">{event.organiser}</p>
             )}
-            <p className="text-sm text-gray-500">
-              {event.eventDate.toLocaleDateString()}
-            </p>
-          </div>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 }
@@ -51,11 +76,11 @@ function Gimmick() {
       <span
         style={{
           position: 'absolute',
-          top: -50,
-          right: 0,
+          top: -30,
+          right: -10,
           display: 'inline-block',
-          width: 180,
-          height: 180,
+          width: 'min(20vw, 180px)',
+          height: 'min(20vw, 180px)',
           backgroundImage: 'url(/illustraties/event-gimmick.svg)',
           backgroundSize: 'contain',
           backgroundRepeat: 'no-repeat',
