f6fead4514a18b86192ed175a24fe1eb86b151ce
Lody Borgers <hi@lodybo.nl>
2026-02-02 17:12:48 +0100

    WAT-53 Created component and block for the talent program details

---
+++ app/config/blocks/TalentProgramBlock.tsx
@@ -0,0 +1,17 @@
+import { ComponentConfig } from '@puckeditor/core';
+import TalentProgram, { Props } from '~/components/TalentProgram';
+
+export type TalentProgramBlockProps = Props;
+
+export const TalentProgramBlock: ComponentConfig<TalentProgramBlockProps> = {
+  label: 'Talent Programma',
+  fields: {
+    programLogoUrl: { type: 'text', label: 'Programma Logo URL' },
+    programLogoAltText: { type: 'text', label: 'Alt-tekst voor het logo' },
+    description: { type: 'text', label: 'Beschrijving' },
+    moreInfoUrl: { type: 'text', label: 'Meer Informatie URL' },
+  },
+  render(props) {
+    return <TalentProgram {...props} />;
+  },
+};
