c7ccc4547ae8c93e32ebf2fd45af699df726f151
Lody Borgers <hi@lodybo.nl>
2026-02-19 14:55:47 +0100

    WAT-144 Restarted the component, using a different approach.

---
+++ app/components/TabComponent.translations.ts
@@ -0,0 +1,14 @@
+// TabComponent doesn't use translations directly since content is passed as props
+// This file is kept for consistency with the component structure pattern
+
+export const en = {
+  TabComponent: {},
+};
+
+export const nl = {
+  TabComponent: {},
+};
+
+export const pap = {
+  TabComponent: {},
+};
--- app/locales/en.ts
+++ app/locales/en.ts
@@ -12,8 +12,6 @@ import { en as AccountMenuTranslations } from '~/routes/($lang)/account/AccountM
 export const { AccountMenu } = AccountMenuTranslations;
 import { en as AdminMenuTranslations } from '~/routes/($lang)/beheer/AdminMenu.translations';
 export const { AdminMenu } = AdminMenuTranslations;
-import { en as ContentTableTranslations } from '~/components/ContentTable.translations';
-export const { ContentTable } = ContentTableTranslations;
 import { en as ConfirmDeleteDialogTranslations } from '~/routes/($lang)/beheer/$content/_index/ConfirmDeleteDialog.translations';
 export const { ConfirmDeleteDialog } = ConfirmDeleteDialogTranslations;
 import { en as DeletionNotificationTranslations } from '~/routes/($lang)/beheer/$content/_index/DeletionNotification.translations';
